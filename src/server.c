00e7f79d4bca958ba8ffd3a7b76bcddf501104d4
Mihai Fufezan <mihai@fufexan.net>
2024-12-01 16:24:39 +0200

    server: track all fds and dispose of them on SIGINT

--- src/server.c
+++ src/server.c
@@ -14,22 +14,24 @@
 #include "client.h"
 #include "shared.h"
 
-#define PORT "22034"
-#define ASSIGNED_COMMAND 4
+// Hold information about the active sockets
+typedef struct {
+  int *active_sockets; // Array of active socket descriptors
+  size_t socket_count; // Number of active sockets
+} resource_tracker;
 
 // Functions only used by the server
 void *handle_connection(void *);
 int get_listener_socket(void);
+void track_sock(resource_tracker *, int);
+void close_all_sockets(resource_tracker *);
+void int_handler(int);
 
-int sockfd;
+// Global variables
+const char PORT[] = "22034";
+const int ASSIGNED_COMMAND = 4;
 bool ALL_COMMANDS = false;
-
-void int_handler(int sig) {
-  check(close(sockfd), "close");
-  printf("\nServer closed\n");
-
-  exit(0);
-}
+resource_tracker tracker = {NULL, 0};
 
 int main(int argc, char **argv) {
   signal(SIGINT, int_handler);
@@ -40,20 +42,25 @@ int main(int argc, char **argv) {
   char *all_commands = getenv("ALL_COMMANDS");
   ALL_COMMANDS = all_commands != NULL;
 
-  sockfd = get_listener_socket();
+  int sockfd = get_listener_socket();
+  track_sock(&tracker, sockfd);
 
   // Get ready to accept a connection
-  int newsockfd;
+  int client_fd;
   struct sockaddr_storage remote_addr;
   socklen_t addr_size = sizeof(remote_addr);
 
   while (true) {
     // Blocks until a connection is initiated
     debug("Accepting connection...");
-    newsockfd =
+    client_fd =
         check(accept(sockfd, (struct sockaddr *)&remote_addr, &addr_size),
               "accept error!");
 
+    // Add client_fd to tracker's active sockets
+    track_sock(&tracker, client_fd);
+
+    // Get string representation of the client's IP
     char remote_ipv4[INET_ADDRSTRLEN];
 
     if (!inet_ntop(remote_addr.ss_family,
@@ -62,14 +69,14 @@ int main(int argc, char **argv) {
       error("Failed to get string representation of remote address");
     }
 
-    printf("New connection from %s on socket %d\n", remote_ipv4, newsockfd);
+    printf("New connection from %s on socket %d\n", remote_ipv4, client_fd);
 
     // Make a pthread
     pthread_t t;
     // pthreads want a void pointer, so we'll cast our fd. This will be freed in
     // handle_connection
     int *threadarg = malloc_s(sizeof(int));
-    *threadarg = newsockfd;
+    *threadarg = client_fd;
     pthread_create(&t, NULL, handle_connection, threadarg);
     // Threads are on their own
     // Even though we detach the thread immediately after creation, valgrind may
@@ -83,7 +90,7 @@ int main(int argc, char **argv) {
 
 // Handle connections initiated by clients. Can be used with pthreads.
 void *handle_connection(void *fd) {
-  int newsockfd = *(int *)fd;
+  int client_fd = *(int *)fd;
   free(fd);
 
   debug("Connection accepted. Waiting for messages...");
@@ -91,7 +98,7 @@ void *handle_connection(void *fd) {
   char *buf = NULL, *response_buf = NULL;
 
   // Keep connection open as long as the client is connected
-  while ((buf = recv_all(newsockfd, 3)) && strlen(buf) != 0) {
+  while ((buf = recv_all(client_fd, 3)) && strlen(buf) != 0) {
     // We only want 3 bytes, in the form "xy#", where x and y are digits
     int cmd = atoi(buf);
     printf("cmd: %s\n", buf);
@@ -105,7 +112,7 @@ void *handle_connection(void *fd) {
     }
 
     // Send response
-    send_all(newsockfd, response_buf, strlen(response_buf));
+    send_all(client_fd, response_buf, strlen(response_buf));
 
     // Free dynamically allocated response_buf
     if (ALL_COMMANDS || cmd == ASSIGNED_COMMAND) {
@@ -119,7 +126,7 @@ void *handle_connection(void *fd) {
     free(buf); // Free if recv_all returned non-NULL
   }
 
-  close(newsockfd);
+  close(client_fd);
   pthread_exit(0);
   return NULL;
 }
@@ -175,4 +182,29 @@ int get_listener_socket(void) {
   check(listen(listener, 10), "Could not start listening");
 
   return listener;
+}
+
+// Close all sockets in a resource_tracker struct
+void close_all_sockets(resource_tracker *tracker) {
+  for (size_t i = 0; i < tracker->socket_count; i++) {
+    check(close(tracker->active_sockets[i]), "close");
+  }
+
+  free(tracker->active_sockets);
+  tracker->active_sockets = NULL;
+}
+
+// SIGINT handler
+void int_handler(int sig) {
+  close_all_sockets(&tracker);
+  printf("\nServer closed\n");
+
+  exit(0);
+}
+
+// Track a socket inside a resource_tracker struct
+void track_sock(resource_tracker *tracker, int fd) {
+  tracker->active_sockets = realloc_s(
+      tracker->active_sockets, (tracker->socket_count + 1) * sizeof(int));
+  tracker->active_sockets[tracker->socket_count++] = fd;
 }
